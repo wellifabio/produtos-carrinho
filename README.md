0aff8c970866b0e602a3038ac6e39a8b6dd320ac
wellifabio <robsonbacchindesouza@gmail.com>
2024-11-13 10:04:00 -0300

    carrinho de compras

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Listar Cards Produtos
-Exemplo de aplicativo web front-end, que lista dados de um vetor JSON e renderiza em forma de **Cards**, mostra os detalhes de cada produto em um **modal** e também melhora a UX (User experience) exibindo mensagens em um modal ao inves de alert().
+# Cards e Carrinho de Compras [Front-end]
+Exemplo de aplicativo web front-end, que lista dados de um vetor JSON e renderiza em forma de **Cards**, mostra os detalhes de cada produto em um **modal**, melhora a UX (User experience) exibindo mensagens em um modal ao inves de alert(). E por fim, adiciona produtos ao carrinho de compras, exibindo a quantidade de itens e o valor total, perminindo a remoção de itens do carrinho e alteração da quantidade de itens.
 
 ## Requisitos
 - [VsCode](https://code.visualstudio.com/)
@@ -8,5 +8,11 @@ Exemplo de aplicativo web front-end, que lista dados de um vetor JSON e renderiz
 1. Clone este repositório
 2. Abra com o VsCode 
 3. Execute o arquivo **index.html** no navegador ou via live server do VsCode.
-A imagem a seguir mostra o as telas no formato responsivo.
-![Print](./prints.png)
+
+## Wireframes
+|Web|Responsivo|
+|-|-|
+|![Tela 01](./wireframes/wireframe1.png)|![Tela 01 responsiva](./wireframes/wireframe1r.png)|
+|![Tela 02](./wireframes/wireframe2.png)|![Tela 02 responsiva](./wireframes/wireframe2r.png)|
+|![Tela 03](./wireframes/wireframe3.png)|![Tela 03 responsiva](./wireframes/wireframe3r.png)|
+|![Tela 04](./wireframes/wireframe4.png)|![Tela 04 responsiva](./wireframes/wireframe4r.png)|
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const produtos = [
         "nome": "Tênis lindão",
         "descricao": "O tênis mais lindo do mundo",
         "preco": 200.00,
-        "peso":0.5,
+        "peso": 0.5,
         "frete": 0.1,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/tenis1.png"
     },
@@ -14,7 +14,7 @@ const produtos = [
         "nome": "Tênis bunitinho",
         "descricao": "O tênis mais bunitinho de hoje",
         "preco": 180.00,
-        "peso":0.5,
+        "peso": 0.5,
         "frete": 0.1,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/tenis2.png"
     },
@@ -23,7 +23,7 @@ const produtos = [
         "nome": "Bruzinha",
         "descricao": "Camiseta branca simples",
         "preco": 49.90,
-        "peso":0.3,
+        "peso": 0.3,
         "frete": 0.1,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/camiseta1.png"
     },
@@ -32,7 +32,7 @@ const produtos = [
         "nome": "Camiseta Preta",
         "descricao": "Camiseta pretinha básica",
         "preco": 59.90,
-        "peso":0.3,
+        "peso": 0.3,
         "frete": 0.1,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/camiseta2.png"
     },
@@ -41,7 +41,7 @@ const produtos = [
         "nome": "Calsa jeans masculino",
         "descricao": "Calsa jeans masculino, azul básico",
         "preco": 49.90,
-        "peso":1.2,
+        "peso": 1.2,
         "frete": 0.2,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/calsa1.png"
     },
@@ -50,11 +50,13 @@ const produtos = [
         "nome": "Calsa jeans feminino",
         "descricao": "Calsa jeans feminino, azul básico",
         "preco": 49.90,
-        "peso":0.9,
+        "peso": 0.9,
         "frete": 0.2,
         "imagem": "https://wellifabio.github.io/produtos-cards/assets/calsa2.png"
     }
 ];
+
+const carrinho = [];
 
 function listarProdutos() {
     let output = "";
@@ -64,7 +66,7 @@ function listarProdutos() {
                 <h3 class="card-title  text-center">${produto.nome}</h3>
                 <img src="${produto.imagem}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <p class="card-text text-center">R$ ${produto.preco.toFixed(2).replaceAll('.',',')}</p>
+                    <p class="card-text text-center">R$ ${produto.preco.toFixed(2).replaceAll('.', ',')}</p>
                 </div>
                 <button class="btn btn-primary" onclick="modalDetalhes(${produto.id})">Detalhes</button>
             </div>
@@ -73,13 +75,14 @@ function listarProdutos() {
     prods.innerHTML += output;
 }
 
-function modalDetalhes(id){
+function modalDetalhes(id) {
     //Usando a função find para encontrar o produto na lista pelo id
     let produto = produtos.find(produto => produto.id === id);
     //Obtendo os elementos do modal e mostrando ele
     const detalhes = document.getElementById("detalhes");
     detalhes.classList.remove("oculto");
     const titulo = document.getElementById("titulo");
+    const idProd = document.getElementById("idProd");
     const img = document.getElementById("img");
     const descricao = document.getElementById("descricao");
     const peso = document.getElementById("peso");
@@ -88,19 +91,90 @@ function modalDetalhes(id){
     const total = document.getElementById("total");
     //Preenchendo os dados
     titulo.innerHTML = produto.nome;
+    idProd.innerHTML = produto.id;
     img.src = produto.imagem;
     descricao.innerHTML = produto.descricao;
-    preco.innerHTML = "Preço R$ " + produto.preco.toFixed(2).replaceAll('.',',');
-    peso.innerHTML = "Peso: " +produto.peso +"kg";
+    preco.innerHTML = "Preço R$ " + produto.preco.toFixed(2).replaceAll('.', ',');
+    peso.innerHTML = "Peso: " + produto.peso + "kg";
     //Fórmula para cálculo do frete (frete * peso * preço)
     let freteDinheiro = produto.frete * produto.peso * produto.preco;
-    frete.innerHTML = "Frete R$ " + freteDinheiro.toFixed(2).replaceAll('.',',');
-    total.innerHTML = "Total R$ " + (freteDinheiro + produto.preco).toFixed(2).replaceAll('.',',');
+    frete.innerHTML = "Frete R$ " + freteDinheiro.toFixed(2).replaceAll('.', ',');
+    total.innerHTML = "Total R$ " + (freteDinheiro + produto.preco).toFixed(2).replaceAll('.', ',');
 }
 
-function modalMensagem(msg){
+function modalMensagem(msg) {
     const mensagem = document.getElementById("mensagem");
     const texto = document.getElementById("texto");
     mensagem.classList.remove("oculto");
     texto.innerHTML = msg;
+}
+
+function adicionarCarrinho(id) {
+    //Verificar se o produto já está no carrinho
+    const produtoCarrinho = carrinho.find(produto => produto.id == id);
+    if (produtoCarrinho) {
+        carrinho.forEach(produto => {
+            if (produto.id == id) {
+                produto.quantidade++;
+                produto.total = produto.quantidade * produto.total;
+            }
+        });
+    } else {
+        const produto = produtos.find(produto => produto.id == id);
+        produto.freteDinheiro = produto.frete * produto.peso * produto.preco;
+        produto.total = produto.freteDinheiro + produto.preco;
+        produto.quantidade = 1;
+        carrinho.push(produto);
+    }
+    preencherTabelaCarrinho();
+    mostrarCarrinho();
+}
+
+function preencherTabelaCarrinho() {
+    const tabela = document.getElementById("corpoCarrinho");
+    let output = "";
+    let total = 0;
+    carrinho.forEach(produto => {
+        output += `
+            <tr>
+                <td>${produto.id}</td>
+                <td>${produto.nome}</td>
+                <td>${produto.descricao}</td>
+                <td><img src="${produto.imagem}"></td>
+                <td><button class="btn btn-danger" onclick="diminuir(${produto.id})">-</button> ${produto.quantidade} <button class="btn btn-danger" onclick="aumentar(${produto.id})">+</button></td>
+                <td>${produto.total.toFixed(2).replaceAll('.', ',')}</td>
+            </tr>
+        `;
+        total += produto.total;
+    });
+    tabela.innerHTML = output;
+    document.getElementById("totalCarrinho").innerHTML = "Total R$ " + total.toFixed(2).replaceAll('.', ',');
+}
+
+function mostrarCarrinho() {
+    const carrinho = document.getElementById("carrinhoCompras");
+    carrinho.classList.remove("oculto");
+}
+
+function diminuir(id) {
+    carrinho.forEach(produto => {
+        if (produto.id == id) {
+            produto.quantidade--;
+            produto.total =  produto.quantidade * (produto.preco + produto.freteDinheiro);
+            if (produto.quantidade == 0) {
+                carrinho.splice(carrinho.indexOf(produto), 1);
+            }
+        }
+    });
+    preencherTabelaCarrinho();
+}
+
+function aumentar(id) {
+    carrinho.forEach(produto => {
+        if (produto.id == id) {
+            produto.quantidade++;
+            produto.total = produto.quantidade * (produto.preco + produto.freteDinheiro);
+        }
+    });
+    preencherTabelaCarrinho();
 }
